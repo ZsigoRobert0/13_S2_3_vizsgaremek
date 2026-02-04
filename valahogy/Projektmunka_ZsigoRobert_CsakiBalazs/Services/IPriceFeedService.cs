b4be93558615401622bae9f9f88330f19e3c3cc0
csakibalazs545 <csaki.balazs@diak.szbi-pg.hu>
2026-02-04 12:05:26 +0100

    tisztítás

--- valahogy/Projektmunka_ZsigoRobert_CsakiBalazs/Services/IPriceFeedService.cs
+++
@@ -1,8 +0,0 @@
-namespace StockMaster.Api.Services
-{
-    public interface IPriceFeedService
-    {
-        decimal GetPrice(string symbol);
-        Task StartAsync(CancellationToken ct = default);
-    }
-}
--- valahogy/Projektmunka_ZsigoRobert_CsakiBalazs/StockMaster/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once "../StockMaster/session.php";
-session_destroy();
-header("Location: login.php");
-exit;
-?>
